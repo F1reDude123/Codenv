206474be27796ad49e6c63d929c1197e1a4cdaab
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-09-04 16:20:25 -0400

    Update main.js

--- vm/main.js
+++ vm/main.js
@@ -1,47 +1,48 @@
-var ramSize = 1024;
-var registerCount = 50;
-var ram = new Uint8Array(ramSize);
-var registers = new Uint8Array(registerCount);
-var ramidx=0;
-
-function start() {
-  var running = true;
-  var i=0;
-  while (running) {
-    switch(ram[i++]) {
-      case 0x00: //NIL
+class VM {
+  ramSize = 1024;
+  registerCount = 50;
+  ram = new Uint8Array(ramSize);
+  registers = new Uint8Array(registerCount);
+  ramidx=0;
+  
+  start() {
+    var i=0;
+    var running = true;
+    while (running) {
+      switch(ram[i++]) {
+        case 0x00: //NIL
+          break;
+        case 0x01: //STR
+          var reg = this.ram[i++];
+          var mem = this.ram[i++];
+          this.registers[reg] = mem;
+          break;
+        case 0x02: //LOD
+          var mem = this.ram[i++];
+          var reg = this.ram[i++];
+          this.ram[mem] = this.registers[reg];
+          break;
+        case 0x0F: //TST
+          alert(Array.from(this.ram.slice(0, 16)));
+          alert(Array.from(this.registers.slice(0, 16)));
+          break;
+        case 0xFF: //STP
+          running = false;
+          break;
+      }
+    }
+  }
+  
+  pushInstruction(code) {
+    while (true) {
+      if (ram[ramidx] == 0) {
+        ram[ramidx] = code;
+        ramidx++;
         break;
-      case 0x01: //STR
-        var reg = ram[i++];
-        var mem = ram[i++];
-        registers[reg] = mem;
-        break;
-      case 0x02: //LOD
-        var mem = ram[i++];
-        var reg = ram[i++];
-        ram[mem] = registers[reg];
-        break;
-      case 0x0F: //TST
-        alert(Array.from(ram.slice(0, 16)));
-        alert(Array.from(registers.slice(0, 16)));
-        break;
-      case 0xFF: //STP
-        running = false;
-        break;
+      }
     }
   }
 }
-
-function pushInstruction(code) {
-  while (true) {
-    if (ram[ramidx] == 0) {
-      ram[ramidx] = code;
-      ramidx++;
-      break;
-    }
-  }
-}
-
 function pushFile(file) {
   var reader = new FileReader();
   reader.onload = function(){ram.set(reader.result.split(" "), 0)}
