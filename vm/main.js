a203f629904486438d128b130f2ec05c8ba8c6df
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-09-06 16:53:43 -0400

    Update main.js

--- vm/main.js
+++ vm/main.js
@@ -1,9 +1,17 @@
 class VM {
   ramSize;
   registerCount;
-  constructor(ramSize = 1024, registerCount = 50) {
+  constructor(ramSize = 1024, registerCount = 64) {
     this.ram = new Uint8Array(ramSize);
     this.registers = new Uint8Array(registerCount);
+    this.gpu = {
+      "enabled":false,
+      "width":0,
+      "height":0,
+      "fbAddr":0
+    }
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
   }
   ramidx=0;
   
@@ -14,35 +22,57 @@ class VM {
       switch(this.ram[i++]) {
         case 0x00: //NIL
           break;
-        case 0x01: //STR
+        case 0x01: //STI
           var reg = this.ram[i++];
-          var mem = this.ram[i++];
-          this.registers[reg] = mem;
+          var val = this.ram[i++];
+          this.registers[reg] = val;
           break;
-        case 0x02: //LOD
+        case 0x02: //STR
+          var reg = this.ram[i++];
+          var val = this.ram[i++];
+          this.registers[reg] = this.ram[val];
+          break;
+        case 0x03: //LDI
+          var mem = this.ram[i++];
+          var val = this.ram[i++];
+          if (mem < 0xF0) {
+            this.ram[mem] = val;
+          }
+          else if (mem < 0xFF) {
+            switch(mem) {
+              case 0xF0:
+                this.gpu["enabled"] = true;
+                break;
+              case 0xF4:
+                this.canvas.width = val;
+                this.gpu["width"] = val;
+                this.gpu["fbAddr"] = this.ram.length-this.gpu["width"]*this.gpu["height"]*4;
+                break;
+              case 0xF8:
+                this.canvas.height = val;
+                this.gpu["height"] = val;
+                this.gpu["fbAddr"] = this.ram.length-this.gpu["width"]*this.gpu["height"]*4;
+                break;
+              case 0xFC:
+                var imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+                var data = imageData.data;
+                var idx=0;
+                var pixels = this.ram.slice(this.gpu["fbAddr"], this.ram.length);
+                for (var pix=0;pix<pixels.length;pix++) {
+                  data[idx++] = pixels[pix];
+                  data[idx++] = pixels[pix];
+                  data[idx++] = pixels[pix];
+                  data[idx++] = 255;
+                }
+                this.ctx.putImageData(imageData, 0, 0);
+                break;
+            }
+          }
+        break;
+        case 0x04: //LDR
           var mem = this.ram[i++];
           var reg = this.ram[i++];
           this.ram[mem] = this.registers[reg];
-          break;
-        case 0x03: //ADD
-          var add1 = this.ram[i++];
-          var add2 = this.ram[i++];
-          this.registers[add1] = this.registers[add1]+this.registers[add2];
-          break;
-        case 0x04: //SUB
-          var sub1 = this.ram[i++];
-          var sub2 = this.ram[i++];
-          this.registers[sub1] = this.registers[sub1]-this.registers[sub2];
-          break;
-        case 0x05: //MUL
-          var mul1 = this.ram[i++];
-          var mul2 = this.ram[i++];
-          this.registers[mul1] = this.registers[mul1]*this.registers[mul2];
-          break;
-        case 0x06: //DIV
-          var div1 = this.ram[i++];
-          var div2 = this.ram[i++];
-          this.registers[div1] = this.registers[div1]/this.registers[div2];
           break;
         case 0x0F: //TST
           alert(Array.from(this.ram.slice(0, 16)));
